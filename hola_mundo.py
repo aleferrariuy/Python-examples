732aef14a72cccd5c33e4fd78e401b5470d42df8
aleferrariuy <aleferrari.uy@gmail.com>
2025-05-06 08:24:43 -0300

    Create hola hola_mundo

---
+++ hola_mundo.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Módulo: nombre_del_script.py
+Descripción: Breve explicación del propósito del script.
+Autor: Alex (o tu nombre)
+Fecha: 2025-05-04
+Versión: 1.0
+"""
+
+import sys
+import os
+from datetime import datetime  
+
+# Funciones principales
+
+# Bloque principal
+
+print("Hola Mundo")
