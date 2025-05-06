08c4810ce15d1e54594552a3e67aaa449be4b6a9
aleferrariuy <aleferrari.uy@gmail.com>
2025-05-06 08:34:42 -0300

    Head

--- hola_mundo.py
+++ hola_mundo.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 
 """
-Módulo: nombre_del_script.py
-Descripción: Breve explicación del propósito del script.
-Autor: Alex (o tu nombre)
+Módulo: hola_mundo.py
+Descripción: Mi primer script que saluda al mundo.
+Autor: Alejandro
 Fecha: 2025-05-04
 Versión: 1.0
 """
---
+++ series_graficar_i.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Módulo: series_graficar_i.py
+Descripción: Jugando con series numéricas y rep gráfica.
+Autor: Alejandro
+Fecha: 2025-05-06
+Versión: 1.0
+"""
+
+import sys
+import os
+from datetime import datetime
+
