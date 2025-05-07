3ca8f3b68beb5b3da904fd04225a6c095776029a
aleferrariuy <aleferrari.uy@gmail.com>
2025-05-07 16:06:16 -0300

    new file

---
+++ m2_control_flujo.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Módulo: m2_control_flujo.py
+Descripción: Control de flujo y estructuras de datos.
+Autor: Alejandro
+Fecha: 2025-05-07
+Versión: 1.0
+"""
+
+# variables
+secc = 1
+
+"""
+if
+"""
+if secc = 1
+    print(f"La sección es la ",secc)
+
+"""
+elif
+"""
+
+"""
+else
+"""
+
+"""
+for
+"""
+
+"""
+while
+"""
